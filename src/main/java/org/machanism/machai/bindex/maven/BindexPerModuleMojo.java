3d14e66065bd648d3304a52a514e2e8b35c1a0ea
ViktorTovstyi <viktor.tovstyi@gmail.com>
2026-09-02 13:36:40 +0300

    fixed BindexPerModuleMojo.

--- src/main/java/org/machanism/machai/bindex/maven/BindexPerModuleMojo.java
+++ src/main/java/org/machanism/machai/bindex/maven/BindexPerModuleMojo.java
@@ -1,10 +1,19 @@
 package org.machanism.machai.bindex.maven;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Settings;
 import org.machanism.machai.gw.maven.AbstractActPerModuleMojo;
+import org.machanism.machai.gw.processor.GWConstants;
 
 /**
  * Maven goal that coordinates Bindex generation for a multi-module reactor.
@@ -29,4 +38,70 @@ public class BindexPerModuleMojo extends AbstractActPerModuleMojo {
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		performAct("bindex");
 	}
+	
+	@Parameter(readonly = true, defaultValue = "${settings}")
+	public void setSettings(Settings settings) {
+		this.settings = settings;
+	}
+
+	/**
+	 * @param session the session to set
+	 */
+	@Parameter(defaultValue = "${session}", readonly = true, required = true)
+	public void setSession(MavenSession session) {
+		this.session = session;
+	}
+
+	/**
+	 * @param model the model to set
+	 */
+	@Parameter(property = GWConstants.MODEL_PROP_NAME)
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	@Parameter(defaultValue = "${basedir}", required = true)
+	public void setBasedir(File basedir) {
+		this.basedir = basedir;
+	}
+
+	@Parameter(property = GWConstants.PATH_PROP_NAME)
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	@Parameter(property = GWConstants.INSTRUCTIONS_PROP_NAME, name = "instructions")
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+
+	@Parameter(property = GWConstants.EXCLUDES_PROP_NAME, name = "excludes")
+	public void setExcludes(String[] excludes) {
+		this.excludes = excludes;
+	}
+
+	@Parameter(readonly = true, defaultValue = "${project}")
+	public void setProject(MavenProject project) {
+		this.project = project;
+	}
+
+	@Parameter(property = SERVERID_PROP_NAME, required = false)
+	public void setServerId(String serverId) {
+		this.serverId = serverId;
+	}
+
+	@Parameter(defaultValue = "${reactorProjects}", readonly = true)
+	public void setReactorProjects(List<MavenProject> reactorProjects) {
+		this.reactorProjects = reactorProjects;
+	}
+
+	public void setParams(Map<String, String> params) {
+		this.params = params;
+	}
+
+	@Parameter(property = GWConstants.CONFIG_PROP_NAME, required = false)
+	public void setConfigFile(File configFile) {
+		this.configFile = configFile;
+	}
+
 }
